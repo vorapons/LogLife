d2caa6f5fa3b192d6e902bee085d301e4d53c1dc
VoraponS <55026103+vorapons@users.noreply.github.com>
2024-03-17 20:11:05 +0700

    Initial Commit

---
+++ LogLife/LogLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogLifeApp.swift
+//  LogLife
+//
+//  Created by Vorapon Sirimahatham on 17/3/24.
+//
+
+import SwiftUI
+
+@main
+struct LogLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
